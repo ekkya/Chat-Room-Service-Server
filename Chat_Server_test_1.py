5dc32dbd8dbfa427de11ae799dea1e9da3305432
Alaa Ekky <ekkya@tcd.ie>
2017-10-15 15:47:46 +0100

    Added functionality for join and broadcasting. A client joining the chat room established

--- Chat_Server_test_1.py
+++ Chat_Server_test_1.py
@@ -3,27 +3,54 @@ import select
 import sys
 from thread import *
 
+Connection_List = []
+Seq_num = 1
+CRN = room1
+Ser_IP = socket.gethostbyname(socket.gethostname())
+Room_Ref = 1
+Join_ID = Seq_num
 serverPort = 81
+
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
-print 'The server is ready to receive'
+serverSocket.listen(10)
+print 'The server is ready to receive on Port' + serverPort
 
-try:
-        while True:
-                connectionSocket, addr = serverSocket.accept()
-                print addr
-                sentence = connectionSocket.recv(1024)
-                capitalizedSentence = sentence.upper()
-                connectionSocket.send(capitalizedSentence)
-finally:        
-        connectionSocket.close()
+Connection_List.append(serverSocket)
+print "Connection List: " + Connection_List
 
-#Join the chat room
+while 1:
+        read_sockets, write_sockets, error_sockets = select.select(Connection_List, [], [])
+
+        for sock in read_sockets:
+                #For New Connections
+                if sock == serverSocket:
+                        sockfc, addr = serverSocket.accept()
+                        Connection_List.append(sockfc)
+                        print "Connection List: " + Connection_List
+                        JOIN(CRN, Ser_IP, serverPort, Room_Ref, Join_ID)
+                        print "Client (%s, %s) connected" % addr
+                        broadcast(sockfc, "[%s:%s] entered chat room\n" % addr)
+                #else:
+                        #Deal with messages coming from the client - including with "Nonsense" messages
+
+
+
+
+
+#Join the chat room - Response sent to client after establishing socket connection
 def JOIN(CRN, Ser_IP, Port, Room_Ref, Join_ID):        
-        Seq_num = 1               #Unique Identifier for Join
-        socket.gethostname() 
-        Ser_IP = socket.gethostbyname(socket.gethostname())  #Server IP
-        Port = 81
-        Join_ID = Seq_num
-        
+        response = ("JOINED CHATROOM:"+str(CRN), Ser_IP, Port, Room_Ref, Join_ID)
+        print "Response sent to Client on first connection: " + response
+
+        return response
+
+#Function to broadcast chat messages to all connected clients
+def broadcast(sock, message):
+        for socket in Connection_List:
+                if socket != serverSocket and socket != sock:
+                        try:
+                                socket.send(message)
+                        except:
+                                socket.close()
+                                Connection_List.remove(socket)
